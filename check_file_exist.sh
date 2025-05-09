ba0314b56afb1b90396e2297a1cd45265da74e16
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-09 17:38:58 +0100

    affiche

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Demande à l'utilisateur de saisir un nom de fichier
-echo "Entrez le nom du fichier : "
+echo "Entrez le nom du fichier :"
 read filename
 # Vérifie si le fichier existe
 if [ -f "$filename" ]; then
